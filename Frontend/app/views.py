e36dd477089cca85752232b2d1ee87ca5a58721a
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-17 13:57:09 -0600

    funcion obtener datos formulario

--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 import requests
+import json
 
-# Create your views here.
+#Endpiont
+enpointlista = 'http://127.0.0.1:4000/'
 
 def index(request):
     return render(request, 'index.html')
+
+def obtenerdatoscrear(request):
+    try:
+        if request.method == 'POST':
+            form = editar(request.POST)
+            if form.is_valid():
+                nombre = form.cleaned_data['crearNombre']
+                print(nombre)
+        
+        return render(request, 'index.html')
+    except Exception as e:
+        return None
 
 def editar(request):
     return render(request, 'editar.html')
