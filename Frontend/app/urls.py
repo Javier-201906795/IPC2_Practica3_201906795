45467feb68c843149f0092b6cf3e30ddaf2fa3b3
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-10-17 14:22:53 -0600

    obtener lista de productos del backend

--- Frontend/app/urls.py
+++ Frontend/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('editar/', views.editar, name='editar')
+    path('editar/', views.editar, name='editar'),
+    path('obtenerlista/', views.obtenerlista, name='obtenerlista'),
 ]
--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -3,22 +3,27 @@ import requests
 import json
 
 #Endpiont
-enpointlista = 'http://127.0.0.1:4000/'
+endpoint = 'http://127.0.0.1:4000/'
 
 def index(request):
     return render(request, 'index.html')
 
-def obtenerdatoscrear(request):
-    try:
-        if request.method == 'POST':
-            form = editar(request.POST)
-            if form.is_valid():
-                nombre = form.cleaned_data['crearNombre']
-                print(nombre)
-        
-        return render(request, 'index.html')
-    except Exception as e:
-        return None
-
 def editar(request):
     return render(request, 'editar.html')
+
+def obtenerlista(request):
+    try:
+        #obtener del backend
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(endpoint, headers=headers)   
+        print(response.status_code, response.text)
+
+       
+
+        return render(request, 'index.html')
+    except Exception as e:
+        print('Error en obtenerlista()\n',e)
+        return render(request, 'index.html')
+
+
+
